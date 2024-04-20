67326788f4d037ffd127356090a98c4004aaa202
Ettore Ciarcia <ettore.ciarcia@sighup.io>
2024-04-20 11:05:33 +0200

    feat: add env variable in python code to set min number of instances for scale down

--- lambda/scale-down/main.py
+++ lambda/scale-down/main.py
@@ -12,21 +12,27 @@ def lambda_handler(event, context):
     # Convert the JSON string to a list of dictionaries
     autoscaling_groups = json.loads(autoscaling_groups_str)
 
+    # Get the minimum instances from the environment variable, if defined
+    min_instances = int(os.environ.get('MIN_INSTANCES', 0))
+
     # Loop through the Auto Scaling Groups
     for group in autoscaling_groups:
         try:
-            # Scale the Auto Scaling Group to 0 instances
+            # Determine the desired capacity based on the presence of MIN_INSTANCES
+            desired_capacity = min_instances if min_instances > 0 else 0
+
+            # Scale the Auto Scaling Group to desired capacity
             response = autoscaling_client.update_auto_scaling_group(
                 AutoScalingGroupName=group['name'],
-                MinSize=0,
-                MaxSize=0,
-                DesiredCapacity=0
+                MinSize=min_instances,
+                MaxSize=0,  # Setting MaxSize to 0 as in your original code
+                DesiredCapacity=desired_capacity
             )
-            print(f"Auto Scaling Group {group['name']} scaled to 0 instances.")
+            print(f"Auto Scaling Group {group['name']} scaled to {desired_capacity} instances.")
         except Exception as e:
             print(f"Error while scaling Auto Scaling Group {group['name']}: {str(e)}")
 
     return {
         'statusCode': 200,
-        'body': 'Auto Scaling Groups scaled to 0 instances successfully.'
+        'body': f"Auto Scaling Groups scaled to {desired_capacity} instances successfully."
     }
